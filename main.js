99243717fed60e5c05b708a841ea98723554ef6a
michaelb2001 <brunco.michael@gmail.com>
2022-01-10 13:43:24 +0100

    prima versione

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const team = [
+let team = [
     {
       name: 'Wayne Barnett',
       role: 'Founder & CEO',
@@ -30,3 +30,44 @@ const team = [
       image: 'barbara-ramos-graphic-designer.jpg',
     },
   ];
+
+document.getElementById("addMemberButton").addEventListener("click",function(){
+
+  let userName = document.getElementById("name").value;
+  let userRole = document.getElementById("role").value;
+  let userPhoto = document.getElementById("image").value;
+  
+  let userObj = {
+    name: userName,
+    role : userRole,
+    image : userPhoto
+  }
+
+  document.querySelector('.team-container').innerHTML += 
+  `<div class="team-card">
+      <div class="card-image">
+          <img src="${userObj.image}" alt="${userObj.name}"/>
+      </div>
+      
+      <div class="card-text">
+          <h3>${userObj.name}</h3>
+          <p>${userObj.role}</p>
+      </div>
+  </div>`;
+});
+
+for(let i = 1; i < team.length ; i++) {
+    document.querySelector('.team-container').innerHTML += 
+        `<div class="team-card">
+            <div class="card-image">
+                <img src="${team[i].image}" alt="${team[i].name}"/>
+            </div>
+            
+            <div class="card-text">
+                <h3>${team[i].name}</h3>
+                <p>${team[i].role}</p>
+            </div>
+        </div>`;
+}
+
+
